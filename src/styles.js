02e2b064e7c088fc394c3354b4ae3e58a2c1f41d
kkopec <s15509@pjwstk.edu.pl>
2021-08-04 15:27:23 +0200

    Small Devices Styling part II

--- src/styles.js
+++ src/styles.js
@@ -6,6 +6,7 @@ grid-template-columns: 10rem 1fr 10rem;
 grid-template-rows: 20rem 1fr;
 
 @media ${(props) => props.theme.breakpoints.sm} {
+  padding-left: 0.8rem;
   display: grid;
   grid-template-columns:  1fr ;
   grid-template-rows: 30rem 1fr;    
